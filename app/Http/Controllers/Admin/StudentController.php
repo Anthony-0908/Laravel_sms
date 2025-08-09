a623d13874c4214cd8eed5361cd9d35b53a55704
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-09 23:59:28 +0800

    students controller

---
+++ app/Http/Controllers/Admin/StudentController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Student;
+use Yajra\DataTables\DataTables;
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.student.index');
+    }
+
+    public function getStudents() 
+    {
+       return DataTables::of(User::query())
+        ->addIndexColumn()
+        ->addColumn('action', function($row){
+            return '<a href="/users/'.$row->id.'/edit" class="btn btn-sm btn-primary">Edit</a>';
+        })
+        ->rawColumns(['action'])
+        ->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+  public function store(Request $request)
+{
+    // Validate and store
+    // $request->validate([
+    //     'name' => 'required|string|max:255',
+    //     'email' => 'required|email|unique:users,email',
+    //     'password' => 'required|min:6',
+    //     'student_id' => 'required|string|unique:students,student_id',
+    // ]);
+
+    // Create user and student record
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+    ]);
+
+    $user = Student::create([
+        'student_id' => $request->student_id,
+    ]);
+
+    return response()->json([
+        'message' => 'Student registered successfully!',
+    ]);
+}
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
