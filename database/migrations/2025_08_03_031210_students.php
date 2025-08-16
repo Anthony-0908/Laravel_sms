f5c500c8c81722cb1e377c8407950a32e0bb2ecb
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-08-17 01:21:18 +0800

    fixed the foreign key referencing

--- database/migrations/2025_08_03_031210_students.php
+++ database/migrations/2025_08_03_031210_students.php
@@ -14,7 +14,15 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table){
             $table->id();
             $table->string('student_id')->unique();
-             $table->timestamps();
+            $table->string('grade');
+            $table->string('section');
+            $table->date('enrollment_date');
+            $table->unsignedBigInteger('status_id');
+            $table->foreign('status_id')->references('id')->on('student_statuses')->onDelete('cascade');
+
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+          
 
         });
     }
