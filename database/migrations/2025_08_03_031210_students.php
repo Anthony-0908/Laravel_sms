749f9d05a1906237f96f51c1f56184ae79f40e42
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-08-17 12:11:40 +0800

    added the user_id foreign key to the user table and refactored it

--- database/migrations/2025_08_03_031210_students.php
+++ database/migrations/2025_08_03_031210_students.php
@@ -11,20 +11,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table){
-            $table->id();
-            $table->string('student_id')->unique();
-            $table->string('grade');
-            $table->string('section');
-            $table->date('enrollment_date');
-            $table->unsignedBigInteger('status_id');
-            $table->foreign('status_id')->references('id')->on('student_statuses')->onDelete('cascade');
+       Schema::create('students', function (Blueprint $table) {
+        $table->id();
+        $table->unsignedBigInteger('user_id')->default(1);
+        $table->string('student_id')->unique();
+        $table->string('grade');               // required
+        $table->string('section');             // required
+        $table->date('enrollment_date');       // required
+        $table->unsignedBigInteger('status_id');
+        $table->timestamps();
 
-            // $table->foreign('user_id')->references('id')->on('users');
-            $table->timestamps();
-          
+        // Foreign keys
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        $table->foreign('status_id')->references('id')->on('student_statuses')->onDelete('cascade');
+    });
 
-        });
     }
 
     /**
