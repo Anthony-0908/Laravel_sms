cc98ef1c9b92ebbb2164a7a68ef5f720362529ef
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-08-17 12:09:20 +0800

    added has factory database function and user_id

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,13 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    use HasFactory;
 
       protected $fillable = [
         'student_id',
+        'grade',
+        'section',
+        'enrollment_date',
+        'status_id',
+        'gender',
+        'user_id',
     ];
     
 
