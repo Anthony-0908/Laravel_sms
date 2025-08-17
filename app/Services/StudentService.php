df55394ed8eaa919a1e2f3f0743d8f178cc1a29e
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-17 23:27:25 +0800

    student service

--- app/Services/StudentService.php
+++ app/Services/StudentService.php
@@ -2,14 +2,28 @@
 
 namespace App\Services;
 
+use App\Models\StudentStatus;
+use App\Models\Student;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 class StudentService
 {
+    protected $studentStatus;
+    protected $student;
+    protected $user;
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(Studentstatus $studentStatus, Student $student , User $user) 
     {
-        //
+        $this->studentstatus = $studentStatus;
+        $this->student = $student;
+        $this->user = $user;
+    }
+
+    public function getAllStudents()
+    {
+        return $this->student->with('user', 'status')->get();   
     }
 }
 ?>
