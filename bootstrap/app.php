9b395a041269f8cd53801779c69f834e94f5cb7e
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-19 23:25:20 +0800

    added the middleware role

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // Alias your middleware for route usage
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
+
+        // You can also append global middleware or add to groups if needed
+        // $middleware->append(\App\Http\Middleware\SomeOtherMiddleware::class);
+        // $middleware->appendToGroup('web', \App\Http\Middleware\RoleMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
