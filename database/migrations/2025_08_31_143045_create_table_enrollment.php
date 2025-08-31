fef107a673a00335f199452b314a2dc1c5eae598
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-31 23:57:16 +0800

    enrollment table

---
+++ database/migrations/2025_08_31_143045_create_table_enrollment.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enrollment', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('class_id');
+            $table->string('grade');
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enrollment');
+    }
+};
