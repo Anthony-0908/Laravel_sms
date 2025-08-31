40699a33114b511de1b9cb2add1006abeec5eff0
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-01 00:32:37 +0800

    subjects migration

--- database/migrations/2025_08_31_142444_create_table_subjects.php
+++ database/migrations/2025_08_31_142444_create_table_subjects.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_subjects', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->id();
             $table->string('courser_name');
             $table->string('course_code')->unique();
-            $table->string('courser_description');
+            $table->string('course_description');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_subjects');
+        Schema::dropIfExists('subjects');
     }
 };
