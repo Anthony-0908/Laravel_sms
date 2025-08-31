e66cbbfa062e9bc47e36fc6fdb291f91c3216350
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-08-31 23:57:05 +0800

    teacher table

---
+++ database/migrations/2025_08_19_055526_create_table_teachers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->default(1);
+            $table->unsignedBigInteger('employee_id')->unique();
+            $table->string('teaching_subject');
+            $table->string('teacher_level');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
