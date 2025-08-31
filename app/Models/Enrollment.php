724c9fafd99b74a95a2e7f23bb1c9a23d16adb35
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-01 00:23:25 +0800

    enrollment model

---
+++ app/Models/Enrollment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Enrollment extends Model
+{
+    protected $fillable = [
+        'student_id',
+        'class_id',
+        'grade',
+        'start_date',
+        'end_date',
+    ];
+}
