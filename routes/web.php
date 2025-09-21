934f78e0e2ffc0ad9969c7c3b659224852deb86f
Anthony-0908 <anthonyyuvega09@gmail.com>
2025-09-21 22:17:05 +0800

    fixed the middleware problem

--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,22 @@ Route::get('/dashboard', function () {
 
 Route::get('/student',[StudentController::class,'index']);
 
-Route::middleware('role:admin')->group(function() {
-    Route::get('/student',[StudentController::class,'index'])->name('students.index');
-    Route::get('/students',[StudentController::class,'getStudents'])->name('students.data');
-    Route::get('/students-create',[StudentController::class,'create'])->name('students.create');
-    Route::get('/students/{id}/edit',[StudentController::class,'edit'])->name('students.edit');
-    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-    Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
-    Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
-    Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
+Route::middleware(['role:admin'])->prefix('students')->group(function () {
+    Route::get('/', [StudentController::class, 'index'])->name('students.index');
+    Route::get('/data', [StudentController::class, 'getStudents'])->name('students.data');
+    Route::get('/create', [StudentController::class, 'create'])->name('students.create');
+    Route::get('/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    Route::post('/', [StudentController::class, 'store'])->name('students.store');
+    Route::delete('/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+    Route::put('/{id}', [StudentController::class, 'update'])->name('students.update');
+    Route::get('/{id}', [StudentController::class, 'show'])->name('students.show');
 });
 
+// Route::middleware(['role:admin'])->group(function () {
+//     Route::get('/students', [StudentController::class,'index'])->name('students.index');
+// });
 Route::middleware('role:teacher')->group(function(){
-
+    
 });
 
 Route::middleware('auth')->group(function () {
